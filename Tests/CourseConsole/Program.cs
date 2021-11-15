1b89e7583271b9a29c1fe4fe175317b15c7e1458
Hedeus <hedeus1987@gmail.com>
2021-11-15 12:24:13 +0200

    Тестовый консольный и WPF проекты

---
+++ Tests/CourseConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CourseConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
