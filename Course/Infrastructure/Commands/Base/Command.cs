6d6102ba5faf83edf7480f27736b7cbff70ef91d
Hedeus <hedeus1987@gmail.com>
2021-11-17 11:10:39 +0200

    Базовый класс команды

--- Course/Infrastructure/Commands/Base/Command.cs
+++ Course/Infrastructure/Commands/Base/Command.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Course.Infrastructure.Commands.Base
 {
-    class Command
+    internal abstract class Command : ICommand
     {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+        
+        public abstract void Execute(object parameter);
+        
     }
 }
