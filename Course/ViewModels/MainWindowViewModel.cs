32ff817dbbc36eec1ca280a955517cb13807ca13
Hedeus <hedeus1987@gmail.com>
2021-11-17 11:00:29 +0200

    Привязка свойства Статус к тексту в Статусбаре

--- Course/ViewModels/MainWindowViewModel.cs
+++ Course/ViewModels/MainWindowViewModel.cs
@@ -27,5 +27,19 @@ namespace Course.ViewModels
         }
 
         #endregion
+
+        #region Status : String - Статус программы
+
+        /// <summary>Статус программы</summary>
+        private string _Status = "Готов!";
+
+        /// <summary>Статус программы</summary>
+        public string Status 
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
+
+        #endregion
     }
 }
